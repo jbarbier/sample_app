dfd880a7a8e69dd8dbd58bfe0a5575f908c8bdbc
Julien Barbier <write0@gmail.com>
2013-04-03 22:04:06 -0400

    Finish sign-in-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
 
 #  before_save { |user| user.email = email.downcase }
   before_save { email.downcase! }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :password, length: { minimum: 6 } # password presence: true was removed to avoid duplicate error messages due to password_digest (has_secure_password)
@@ -23,4 +24,11 @@ class User < ActiveRecord::Base
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  private
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
+  
 end
